4226f963b584a3fd681e37ec2b9a539212f9aa17
Maricio Harris <maricioharris@gmail.com>
2021-07-13 11:52:19 -0400

    modified css on shop profile page.

--- react-app/src/components/BarberShopComponent/index.js
+++ react-app/src/components/BarberShopComponent/index.js
@@ -11,56 +11,65 @@ export default function Barbershop() {
   const [barberId, setBarberId] = useState(null);
   const [isHidden, setIsHidden] = useState(false);
 
-
   const barbershop = useSelector((state) => state.barbershops.barbershop);
 
   useEffect(() => {
     dispatch(getBarberShop(barbershopId));
   }, [barbershopId, dispatch]);
 
-  const openForm = (e, barberId) => {
-    setIsHidden(false);
-    setBarberId(barberId);
-  };
+  // const openForm = (e, barberId) => {
+  //   setIsHidden(false);
+  //   setBarberId(barberId);
+  // };
   const onSubmit = () => {
     history.push(`/appointments/${barbershopId}`);
   };
 
   return (
-    <div className="shop-container">
-      <div className="shop-pic"></div>
-      {barbershop && (
-
-        <div className="info-container">
-          <h1>{barbershop.name}</h1>
-          <div>{barbershop.address1}</div>
-          <div>{barbershop.address2}</div>
-          <div>
-            {barbershop.city}, {barbershop.state}, {barbershop.postalCode}
+    <section className="shop-profile">
+      <div className="shop-container">
+        <div className="shop-pic"></div>
+        {barbershop && (
+          <div className="info-container">
+            <h1>{barbershop.name}</h1>
+            <div>{barbershop.address1}</div>
+            <div>{barbershop.address2}</div>
+            <div>
+              {barbershop.city}, {barbershop.state}, {barbershop.postalCode}
+            </div>
+            <div>{barbershop.phoneNumber}</div>
+            <div>{barbershop.operationHours}</div>
+            <div>Website:{barbershop.website}</div>
           </div>
-          <div>{barbershop.phoneNumber}</div>
-          <div>{barbershop.operationHours}</div>
-          <div>Website:{barbershop.website}</div>
+        )}
+        <div className="feature-links__container">
+          <NavLink
+            to={"/appointments/:barbershopId"}
+            className="appt-btn"
+            onClick={onSubmit}
+          >
+            Shedule an Appointment
+          </NavLink>
+          {barbershop && (
+            <NavLink
+              to={`/barbershops/${barbershop.id}/create-review`}
+              className="review-btn"
+              onClick={onSubmit}
+            >
+              Write a Review
+            </NavLink>
+          )}
         </div>
-      )}
-      <NavLink to={"/appointments/:barbershopId"} className="appt-btn" onClick={onSubmit}>
-        Shedule an Appointment
-      </NavLink>
-      {barbershop && (
-        <NavLink to={`/barbershops/${barbershop.id}/create-review`} className="review-btn" onClick={onSubmit}>
-          Write a Review
-        </NavLink>
-
-      )}
-      <div>
-        {barbershop?.barbershop?.barbers?.map((barber) => (
-          <div>
-            {barber.name}
-            <div onClick={(e) => openForm(e, barber.id)}>{barber.name}</div>
-            <div>{barber.id}</div>
-          </div>
-        ))}
+        {/* <div>
+          {barbershop?.barbershop?.barbers?.map((barber) => (
+            <div>
+              {barber.name}
+              <div onClick={(e) => openForm(e, barber.id)}>{barber.name}</div>
+              <div>{barber.id}</div>
+            </div>
+          ))}
+        </div> */}
       </div>
-    </div>
+    </section>
   );
 }
